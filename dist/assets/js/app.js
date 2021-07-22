c0c3f70ed8ae9be19de5f82f77862c730ce45758
Serhii Honchar <sergeygonchar88@gmail.com>
2021-07-22 17:02:49 +0300

    WIP

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,17 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     class LightSelect{
-        static #elements = null;
 
         static setHandlers(instance){
 
             const {renderedElement, element} = instance;
             const title = renderedElement.querySelector('.lightSelect__title');
-            const titleText = renderedElement.querySelector('.lightSelect__title-text');
             const list = renderedElement.querySelector('.lightSelect__list');
-            const listItems = renderedElement.querySelectorAll('.lightSelect__list-item');
-            const searchElem = renderedElement.querySelector('.lightSelect__search-control');
 
+            // toggling dropdown visibility
             title.addEventListener('click', () => {
                 if(instance.isOpened){
                     instance.hide();
@@ -20,63 +17,70 @@ document.addEventListener("DOMContentLoaded", function () {
                 }
             });
 
+            // setting LightSelect value when list item was clicked
             list.addEventListener('click', e => {
                 if(e.target.classList.contains('lightSelect__list-item')){
-                    instance.setValue(e.target.dataset.index);
+                    instance.setActiveOption(+e.target.dataset.index);
                     instance.hide();
                 }
-            })
+            });
 
+            // setting LightSelect value when source <select> was changed
             element.addEventListener('change', () => {
-                instance.setValue(element.selectedIndex);
-            })
+                instance.setActiveOption(element.selectedIndex);
+            });
 
+            // method which will be handle search input event by callback
+            if(instance.isSearch){
+                instance.onSearchInput = function(callback) {
+                    if(typeof callback === 'function'){
+                        const searchElem = renderedElement.querySelector('.lightSelect__search-control');
+                        searchElem.addEventListener('input', callback);
+                    }
+                    // here can be search input callback bu default if callback was not provided
+                }
+            }
+
+            // closing a dropdown if click event detected elsewhere but not on lightSelect DOM-elements
             window.addEventListener('click', () => instance.hide());
             renderedElement.addEventListener('click', (e) => e.stopPropagation());
         }
 
-        constructor({selector, searchPanel = false}) {
+        constructor({selector, isSearch = false}) {
             this.element = null;
             this.renderedElement = null;
-            this.searchPanel = searchPanel;
+            this.isSearch = isSearch;
             this.isOpened = false;
             this.init(selector);
         }
 
         init(selector){
-            LightSelect.#elements = document.querySelectorAll(selector); // is necessary ?
+            const selectEls = document.querySelectorAll(selector);
 
-            LightSelect.#elements.forEach(element => {
+            selectEls.forEach(element => {
 
-                // store an element to LightSelect instance
+                // storing an element to LightSelect instance
                 this.element = element;
                 this.element.style.display = 'none';
 
-                // store an rendered element to LightSelect instance
-                this.renderedElement = this.#render(this);
+                // rendering and storing rendered element to LightSelect instance
+                this.renderedElement = this.render();
                 this.renderedElement.LightSelect = this;
 
+                // setting activeIndex to LightSelect DOM-element
+                this.setActiveOption(this.element.selectedIndex);
 
-                this.setValue(this.element.selectedIndex);
-
-                if(this.searchPanel){
-                    this.onSearchInput = function(callback) {
-                        const searchElem = this.renderedElement.querySelector('.lightSelect__search-control');
-                        searchElem.addEventListener('input', callback);
-                    }
-                }
-
+                // installation of handlers for basic functional elements
                 LightSelect.setHandlers(this);
             });
         }
 
-        #render(instance){
+        render(){
 
-            const elementOptions = instance.element.querySelectorAll('option');
-
-            const lightSelectContainer = document.createElement('div');
-            lightSelectContainer.classList.add('lightSelect');
-            lightSelectContainer.innerHTML = `
+            // creating main wrapper
+            const wrapperEl = document.createElement('div');
+            wrapperEl.classList.add('lightSelect');
+            wrapperEl.innerHTML = `
                 <div class="lightSelect__title">
                     <div class="lightSelect__title-text"></div>
                     <div class="lightSelect__arrow">
@@ -88,47 +92,51 @@ document.addEventListener("DOMContentLoaded", function () {
                 </div>
             `;
 
-            const lightSelectTitleText = lightSelectContainer.querySelector('.lightSelect__title-text');
-            const lightSelectDropdown = lightSelectContainer.querySelector('.lightSelect__dropdown');
-            const lightSelectList = lightSelectContainer.querySelector('.lightSelect__list');
+            // getting option elements from source <select> DOM-element
+            const optionsCollection = this.element.querySelectorAll('option');
 
-            elementOptions.forEach((option, index) => {
+            // getting dropdown element and list element from main wrapper which was created above
+            const dropdownEl = wrapperEl.querySelector('.lightSelect__dropdown');
+            const optionsListEl = dropdownEl.querySelector('.lightSelect__list');
 
-                if(option.selected){
-                    lightSelectTitleText.innerHTML = option.textContent;
-                }
-
-                lightSelectList.innerHTML += `
-                    <div class="lightSelect__list-item" data-selected="${option.selected}" data-value="${option.value}" data-index="${index}">${option.textContent}</div>
+            // appending option elements values to list items
+            optionsCollection.forEach((option, index) => {
+                optionsListEl.innerHTML += `
+                    <div class="lightSelect__list-item" data-value="${option.value}" data-index="${index}">${option.textContent}</div>
                 `;
             })
 
-            instance.element.after(lightSelectContainer);
+            // appending main wrapper after source <select> DOM-element
+            this.element.after(wrapperEl);
 
-            if(instance.searchPanel){
+            // appending search panel into dropdown element
+            if(this.isSearch){
                 const searchPanel = document.createElement('div');
                 searchPanel.classList.add('lightSelect__search-panel');
-                searchPanel.innerHTML = `<input class="lightSelect__search-control" type="text" placeholder="Поиск">`
+                searchPanel.innerHTML = `<input class="lightSelect__search-control" type="text" placeholder="Поиск">`;
 
-                lightSelectDropdown.prepend(searchPanel);
+                dropdownEl.prepend(searchPanel);
             }
-            return lightSelectContainer;
+
+            return wrapperEl;
         }
 
-        setValue(index){
+        setActiveOption(index){
+            const title = this.renderedElement.querySelector('.lightSelect__title-text');
             const listElements = this.renderedElement.querySelectorAll('.lightSelect__list-item');
-            const selectedElement = listElements[+index];
+            const selectedElement = listElements[index];
 
+            // removing activity class from all LigthSelect list item
             listElements.forEach(element => {
                 element.classList.remove('lightSelect__list-item--selected');
-                element.dataset.selected = 'false'
             });
 
-            selectedElement.dataset.selected = 'true';
+            // add activity class to selected LightSelect list item and setting title text value
             selectedElement.classList.add('lightSelect__list-item--selected');
+            title.textContent = selectedElement.textContent;
 
-            this.renderedElement.querySelector('.lightSelect__title-text').textContent = selectedElement.textContent;
-            this.element.selectedIndex = +index;
+            // changing source <select> DOM-element state
+            this.element.selectedIndex = index;
         }
 
         update(updateArr){
@@ -150,7 +158,7 @@ document.addEventListener("DOMContentLoaded", function () {
             this.renderedElement.querySelector('.lightSelect__dropdown').classList.add('lightSelect__dropdown--visible');
             this.isOpened = true;
 
-            if(this.searchPanel){
+            if(this.isSearch){
                 this.renderedElement.querySelector('.lightSelect__search-control').focus();
             }
         }
@@ -160,21 +168,27 @@ document.addEventListener("DOMContentLoaded", function () {
             this.renderedElement.querySelector('.lightSelect__dropdown').classList.remove('lightSelect__dropdown--visible');
             this.isOpened = false;
 
-            if(this.searchPanel){
+            if(this.isSearch){
                 this.renderedElement.querySelector('.lightSelect__search-control').value = "";
             }
+        }
+
+        destroy(){
+            this.element.style = "";
+            this.renderedElement.remove();
+            delete this.element.LightSelect;
         }
 
     }
 
     const elems = new LightSelect({
         selector: '.light-select',
-        searchPanel: true,
+        isSearch: true,
     });
 
-    elems.onSearchInput(function () {
-        console.log('custom search');
-    })
+    // elems.onSearchInput(function () {
+    //     console.log('custom search');
+    // })
 });
 
 
